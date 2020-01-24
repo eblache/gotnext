701d4c2cac77130e1382725a257d580b2f74f2a8
eblache <30540432+eblache@users.noreply.github.com>
2020-01-23 20:32:55 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const myUrl = "https://jsonbox.io/box_39ae5ad169db04082e9c";
+var myData = '{"name":"Drake"}';
+
+function readData(){
+  $.ajax({
+    url: myUrl,
+    type: 'GET',
+    dataType: 'json',
+    success: (data)=>{
+      displayHtml(data);
+    }
+  });
+}
+function loadData(){
+  $.ajax({
+    url: myUrl,
+    type: 'POST',
+    contentType: 'application/json',
+    data: myData,
+    success: function(data){
+      console.log("Success!");
+      console.log(data);
+    },
+    error: function(error){
+      console.log(error);
+      console.log("Nope!");
+    }
+  });
+}
+function displayHtml(data){
+  data.forEach((person)=>{
+    $("#team1 ul").append("<li>"+person.name+"</li>");
+        console.log(person.name);
+    });
+}
+$(document).ready(function(){
+      readData();
+});
+
+
+
+
